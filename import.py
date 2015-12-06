f6eb5e9d5a951deb71ee02d4180426305d65b0fd
Harry Reeder <harry@harryreeder.co.uk>
2015-12-06 05:41:42 +0000

    Improved import script

--- import.py
+++ import.py
@@ -5,13 +5,28 @@ from redis import Redis
 
 r = Redis()
 
-with open(sys.argv[1], 'r') as drillhallcsv:
-    dhreader = csv.reader(drillhallcsv)
-    for rownumber, row in enumerate(dhreader):
+# Arg1: CSV file to import
+# Arg2: -a if you want to push instead of update
+# Arg3: List to push onto, by name
+
+with open(sys.argv[1], 'r') as placecsv:
+    preader = csv.reader(placecsv)
+
+    try:
+        if sys.argv[2] == "-a":
+            r.hset(sys.argv[3], "name", sys.argv[4])
+            r.hset(sys.argv[3], "key", sys.argv[3])
+    except IndexError:
+        pass
+    
+    for rownumber, row in enumerate(preader):
         for colnumber, machine in enumerate(row):
-            #if sys.argv[2] == "-a":
-            #    if machine != "":
-            #        r.lpush('drillhall-machines',machine)
+            try:
+                if sys.argv[2] == "-a":
+                    if machine != "":
+                        r.lpush(sys.argv[3],machine)
+            except IndexError:
+                pass
             r.hmset(machine, {
                 'hostname': machine,
                 'col': colnumber,
