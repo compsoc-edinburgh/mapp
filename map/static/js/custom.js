compsoc-edinburgh/mapp

commit 0a0f94a291f939bc075f81bee6110079ec72541e

Remove ready() call when refreshing
This causes all events to be attached to buttons again, causing further
refreshes to be triggered multiple times (exponentially?) for each
subsequent refresh attempt.

This call isn't actually necessary as none of the JavaScript currently
affects the main display pane.

map/static/js/custom.js
@@ -92,7 +92,6 @@ function readyFunction(){
             .done(function(data){
                 $('#ajax-map-replace').replaceWith(data);   
                 centreMap();
-                readyFunction();
             });
     };
     var createRefreshAlert = function (status) {

commit e6cfa48df94dcbec34d27cea0d37181a0450b149

Fix dragging after refresh (caused by 0a0f94a291f)

map/static/js/custom.js
@@ -86,12 +86,17 @@ function readyFunction(){
         myDiv.scrollLeft(iPosX - (e.pageX - clickX));
     };
     var mapUpdate = function(){
+        var parts = location.pathname.split('/');
+        var site = parts.pop() || parts.pop();  // handle potential trailing slash
+
         $.ajax({ 
-            url: '/api/refresh' 
+            url: `/api/refresh?site=${site}`
         })
             .done(function(data){
                 $('#ajax-map-replace').replaceWith(data);   
                 centreMap();
+                loadMapScroll();
+                refreshData();
             });
     };
     var createRefreshAlert = function (status) {
@@ -123,19 +128,41 @@ function readyFunction(){
                 }
             });       
     };
+    var loadMapScroll = function() {
+        $("#mapscroll").on({
+            'mousemove': function(e) {
+                clicked && updateScrollPos(e);
+            },
+            'mousedown': function(e) {
+                if(e.which == 1){
+                    clicked = true;
+                    clickY = e.pageY;
+                    clickX = e.pageX;
+                    iPosY = $("#mapscroll").scrollTop();
+                    iPosX = $("#mapscroll").scrollLeft();
+                }
+            },
+            'mouseup': function() {
+                clicked = false;
+                $('html').css('cursor', 'auto');
+            }
+        });
+    }
 
-    /* Execute, self */
-    $.ajax({
-        url: '/api/friends'
-    }).done(function(data) {
-        renderFriendList(data);
-    });
-    
-    $.ajax({
-        url: '/api/rooms'
-    }).done(function(data) {
-        renderRoomList(data);
-    });
+    var refreshData = function() {
+        /* Execute, self */
+        $.ajax({
+            url: '/api/friends'
+        }).done(function(data) {
+            renderFriendList(data);
+        });
+        
+        $.ajax({
+            url: '/api/rooms'
+        }).done(function(data) {
+            renderRoomList(data);
+        });
+    }
 
     centreMap();
 
@@ -163,24 +190,9 @@ function readyFunction(){
     $removeButton.on('click',function(){
         $(this).blur();
     });
-    $("#mapscroll").on({
-        'mousemove': function(e) {
-            clicked && updateScrollPos(e);
-        },
-        'mousedown': function(e) {
-            if(e.which == 1){
-                clicked = true;
-                clickY = e.pageY;
-                clickX = e.pageX;
-                iPosY = $("#mapscroll").scrollTop();
-                iPosX = $("#mapscroll").scrollLeft();
-            }
-        },
-        'mouseup': function() {
-            clicked = false;
-            $('html').css('cursor', 'auto');
-        }
-    });
+    loadMapScroll();
+    refreshData();
+
     /* form handling ajaxes */
     $('#del-form').on('submit',function(e){
         e.preventDefault();