5e6fc1e7929da5e074ea6687bd389d2f7be18a58
Qais Patankar <qaisjp@gmail.com>
2018-04-16 22:28:33 +0100

    Remove rooms list from about page

--- map/views.py
+++ map/views.py
@@ -185,12 +185,7 @@ def index():
 
 @app.route("/about")
 def about():
-    rooms = None
-    if current_user.is_authenticated:
-        rooms = map(lambda name: flask_redis.hgetall(name), flask_redis.smembers("forresthill-rooms"))
-        rooms.sort(key=lambda x: x['key'])
-
-    return render_template("about.html", rooms=rooms)
+    return render_template("about.html")
 
 @app.route('/site/<which>', methods=['GET', 'POST'])
 @login_required
